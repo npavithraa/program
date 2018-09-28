18efef14425f4f2ad0661f42180400ae8d61894f
npavithraa <42268661+npavithraa@users.noreply.github.com>
2018-09-28 15:07:46 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int number,i,sum=0;
+printf("\nenter the integer value\n");
+scanf("%d",$number);
+  for(i=0;i<=number;i++);
+   {
+     sum=sum+i;
+   }
+   printf("sum of natural number=%d",sum);
+   return 0;
+   }
+
+
+  output:
+  enter any the integer value 5
+  sum of natural numbers=15
